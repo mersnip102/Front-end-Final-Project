6858b425c032178e9b95f30f315bd3e153b72a86
Nguyễn Xuân Quyến <69411567+mersnip102@users.noreply.github.com>
2023-06-21 17:26:47 +0700

    update profile

--- src/app/pages/profile-student/profile-student.component.ts
+++ src/app/pages/profile-student/profile-student.component.ts
@@ -333,7 +333,8 @@ export class ProfileStudentComponent {
     "Bình Thuận", "Cà Mau", "Cao Bằng", "Đắk Lắk", "Đắk Nông", "Điện Biên", "Đồng Nai", "Đồng Tháp", "Gia Lai", "Hà Giang", "Hà Nam", "Hà Tĩnh", "Hải Dương", "Hậu Giang", "Hòa Bình", "Hưng Yên", "Khánh Hòa", "Kiên Giang", "Kon Tum", "Lai Châu", "Lâm Đồng", "Lạng Sơn", "Lào Cai", "Long An", "Nam Định", "Nghệ An", "Ninh Bình", "Ninh Thuận", "Phú Thọ", "Quảng Bình", "Quảng Nam", "Quảng Ngãi", "Quảng Ninh", "Quảng Trị", "Sóc Trăng", "Sơn La", "Tây Ninh", "Thái Bình", "Thái Nguyên", "Thanh Hóa", "Thừa Thiên Huế", "Tiền Giang", "Trà Vinh", "Tuyên Quang", "Vĩnh Long", "Vĩnh Phúc", "Yên Bái", "Phú Yên", "Cần Thơ", "Đà Nẵng", "Hải Phòng", "Hà Nội", "TP HCM"];
 
   ngOptionsHightSchool = ["An Giang", "Bà Rịa - Vũng Tàu", "Bắc Giang"]
-
+  ngOptionsSemester = ["Spring", "Summer", "Fall"]
+  ngDropdownSemester = "Spring";
 
   onClickImage(imageURL: any): void {
     const image = [
@@ -1036,6 +1037,7 @@ export class ProfileStudentComponent {
     this.cdr.detectChanges();
 
   }
+  dateFormat = 'dd/MM/yyyy';
 
 
   // onFileSelected(event: any) {
